e62e78d6e4097ec8cd37219c83e2f5c0e5c79d64
Christian Berg <berg.christian@gmail.com>
2011-07-07 08:30:06 +0200

    Initial commit after lein new

---
+++ src/toptwen/core.clj
@@ -0,0 +1,1 @@
+(ns toptwen.core)
---
+++ test/toptwen/test/core.clj
@@ -0,0 +1,6 @@
+(ns toptwen.test.core
+  (:use [toptwen.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
